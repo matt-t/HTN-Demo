8f12745b6fbc7f52b79315d7756e1053f0024b8f
Matthew Tam <mtam1222@gmail.com>
2020-02-19 18:15:49 -0500

    asdf

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -38,22 +38,7 @@ export default class PathFinding extends Component {
           if (responseData.type === 'organizer') {
             document.getElementById('extra').innerHTML = 'Phone Number: ' + responseData.phone_number;
           }
-          /*
-          id: number;
-          name: string;
-          profile_pic: string; // a url to an image
-          bio?: string; // a paragraph describing the attendee
-          type: AttendeeType;
-          checked_in: boolean;
-          actions: Action[];
-          num_workshops_attended?: number; // all "hacker" type attendees (and no other types) will have this field
-          sponsor_company?: string; // all "sponsor" type attendees (and no other types) will have this field
-          sponsor_company_link?: string; // all "sponsor" type attendees (and no other types) will have this field
-          next_shift?: number; // datetime (ms); all "volunteer" type attendees (and no other types) will have this field
-          phone_number?: string; // all "organizer" type attendees (and no other types) will have this field
-          */
-        }
-        
+        } 
       })
     }
 
